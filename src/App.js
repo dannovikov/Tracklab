6ffde0b7c31048b12a0a4e11a27a8ae36a7cd9bc
Ines Mustedanagic <inesmustedanagic6@gmail.com>
2023-10-24 18:37:40 -0400

    integrating firebase database

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ import React, { useState, useEffect, useCallback } from 'react';
 import Track from './Track/track';
 
 
+
+// This is the import statements to make firebase work
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+// This is the key
+const firebaseConfig = {
+    apiKey: "AIzaSyBwZZ2QtjU0OivX0PTgSmJlDwg2JsVcYPA",
+    authDomain: "loopnote-7cd85.firebaseapp.com",
+    projectId: "loopnote-7cd85",
+    storageBucket: "loopnote-7cd85.appspot.com",
+    messagingSenderId: "342468166695",
+    appId: "1:342468166695:web:cd8edfa9558969e315ba02",
+    measurementId: "G-E8TJGBFT9K"
+};
+
+// This is the initializing the firebase to work
+// Currently no structure, BUT the firebase key corresponds and does NOT cause error
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+
+
 function App() {
     const [tracks, setTracks] = useState([]);
     const [trackIDCounter, setTrackIDCounter] = useState(0);
@@ -45,11 +69,11 @@ function App() {
     const solo = useCallback((id) => {
         if (!soloActivated || soloTrackID !== id) {
             for (let i = 0; i < tracks.length; i++) {
-                if (tracks[i].id !== id) { 
+                if (tracks[i].id !== id) {
                     // Mute all other tracks
                     tracks[i].setIsMuted(true);
                     tracks[i].setIsSolo(false);
-                } else { 
+                } else {
                     // Unmute the soloed track
                     tracks[i].setIsMuted(false);
                     tracks[i].setIsSolo(true);
@@ -109,7 +133,7 @@ function App() {
     return (
         <div className="App">
             {tracks.map(track => (
-                <Track key={track.id} id={track.id} audioFile={track.audioFile} solo={solo} registerTrack={registerTrack} globalVolume={globalVolume}/>
+                <Track key={track.id} id={track.id} audioFile={track.audioFile} solo={solo} registerTrack={registerTrack} globalVolume={globalVolume} />
             ))}
             <button className="rewind" onClick={setPlayHeadToStart}>&lt;&lt;</button>
             <button className="playall" onClick={playAll}>{allPlaying ? 'Pause All' : 'Play All'}</button>
